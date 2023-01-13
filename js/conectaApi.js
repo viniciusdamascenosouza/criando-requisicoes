f1699d56096a7d804c47b3bcfe21c9caf9c4adfb
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2023-01-13 08:39:36 -0300

    Projeto final

--- js/conectaApi.js
+++ js/conectaApi.js
@@ -18,13 +18,23 @@ async function criaVideo(titulo, descricao, url, imagem) {
             imagem: imagem
         })
     });
-
+    if (!conexao.ok) {
+        throw new Error("Não foi possível enviar o vídeo")
+    }
     const conexaoConvertida = conexao.json();
+
+    return conexaoConvertida;
+}
+
+async function buscaVideo(termoDeBusca) {
+    const conexao = await fetch(`http://localhost:3000/videos?q=${termoDeBusca}`);
+    const conexaoConvertida = await conexao.json();
 
     return conexaoConvertida;
 }
 
 export const conectaApi = {
     listaVideos,
-    criaVideo
+    criaVideo,
+    buscaVideo
 }
