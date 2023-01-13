7745c9e1f83f5c3f24041194a0eb37a3d6b1e44f
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2023-01-13 07:53:13 -0300

    aula3

--- js/conectaApi.js
+++ js/conectaApi.js
@@ -5,7 +5,7 @@ async function listaVideos () {
 }
 
 async function criaVideo(titulo, descricao, url, imagem) {
-    const conexao = await fetch ("http://localhost:3000/videos", {
+    conexao = await fetch ("http://localhost:3000/videos", {
         method: "POST",
         headers: {
             "Content-type": "application/json"
